986178dbb983dd31aab3077cd67367f172024928
Sitch <michael.sitchenko@gmail.com>
2021-06-25 17:13:11 -0400

    better regex

--- editor_plugin.py
+++ editor_plugin.py
@@ -11,38 +11,43 @@ import re
 import shlex
 import subprocess
 from terminatorlib import plugin, config
+from typing import Optional
+from pprint import pprint
 
-AVAILABLE = ['EditorPlugin']
+AVAILABLE = ["EditorPlugin"]
 
-DEFAULT_COMMAND = 'gvim --remote-silent +{line} {filepath}'
-DEFAULT_REGEX = r'([^ \t\n\r\f\v:]+?):([0-9]+)'
-DEFAULT_GROUPS = 'file line'
+DEFAULT_COMMAND = "gvim --remote-silent +{line} {filepath}"
+DEFAULT_REGEX = r"([^ \t\n\r\f\v:]+?):([0-9]+)"
+DEFAULT_GROUPS = "file line"
 DEFAULT_OPEN_IN_CURRENT_TERM = False
+
 
 def to_bool(val):
     return val == "True"
 
+
 class EditorPlugin(plugin.URLHandler):
-    """ Process URLs returned by commands. """
-    capabilities = ['url_handler']
-    handler_name = 'editorurl'
-    nameopen = 'Open File'
-    namecopy = 'Copy Open Command'
+    """Process URLs returned by commands."""
+
+    capabilities = ["url_handler"]
+    handler_name = "editorurl"
+    nameopen = "Open File"
+    namecopy = "Copy Open Command"
 
     def __init__(self):
         self.plugin_name = self.__class__.__name__
         self.config = config.Config()
         self.check_config()
-        self.match = self.config.plugin_get(self.plugin_name, 'match')
-        if hasattr(self.match, 'decode'):
-            self.match = self.match.decode('string_escape')
+        self.match = self.config.plugin_get(self.plugin_name, "match")
+        if hasattr(self.match, "decode"):
+            self.match = self.match.decode("string_escape")
 
     def check_config(self):
         config = {
-            'command': DEFAULT_COMMAND,
-            'match': DEFAULT_REGEX,
-            'groups': DEFAULT_GROUPS,
-            'open_in_current_term': DEFAULT_OPEN_IN_CURRENT_TERM,
+            "command": DEFAULT_COMMAND,
+            "match": DEFAULT_REGEX,
+            "groups": DEFAULT_GROUPS,
+            "open_in_current_term": DEFAULT_OPEN_IN_CURRENT_TERM,
         }
         saved_config = self.config.plugin_get_config(self.plugin_name)
         if saved_config is not None:
@@ -56,45 +61,45 @@ class EditorPlugin(plugin.URLHandler):
         # plugins, we need to use the inspect module to climb up the stack to
         # the Terminal object and call get_cwd() from there.
         for frameinfo in inspect.stack():
-            frameobj = frameinfo[0].f_locals.get('self')
-            if frameobj and frameobj.__class__.__name__ == 'Terminal':
+            frameobj = frameinfo[0].f_locals.get("self")
+            if frameobj and frameobj.__class__.__name__ == "Terminal":
                 return frameobj
 
     def get_cwd(self):
-        """ Return current working directory. """
+        """Return current working directory."""
         term = self.get_terminal()
         if term:
             return term.get_cwd()
 
     def open_url(self):
-        """ Return True if we should open the file. """
+        """Return True if we should open the file."""
         # HACK: Because the plugin doesn't tell us we should open or copy
         # the command, we need to climb the stack to see how we got here.
-        return inspect.stack()[3][3] == 'open_url'
+        return inspect.stack()[3][3] == "open_url"
 
     def callback(self, strmatch):
         cwd = self.get_cwd()
-        libdir = self.config.plugin_get(self.plugin_name, 'libdir')
+        libdir = self.config.plugin_get(self.plugin_name, "libdir")
         config = self.config.plugin_get_config(self.plugin_name)
 
         filepath, line, column = findmatch(config, cwd, libdir, strmatch)
 
         if filepath:
-            command = self.config.plugin_get(self.plugin_name, 'command')
-            command = command.replace('{filepath}', filepath)
-            command = command.replace('{line}', str(line))
-            command = command.replace('{column}', str(column))
+            command = self.config.plugin_get(self.plugin_name, "command")
+            command = command.replace("{filepath}", filepath)
+            command = command.replace("{line}", str(line))
+            command = command.replace("{column}", str(column))
             if self.open_url():
-                if self.config.plugin_get(self.plugin_name, 'open_in_current_term'):
-                    self.get_terminal().feed(command + '\n')
+                if self.config.plugin_get(self.plugin_name, "open_in_current_term"):
+                    self.get_terminal().feed(command + "\n")
                 else:
                     subprocess.call(shlex.split(command))
-                return '--version'
+                return "--version"
             return command
 
 
-def search_filepath_in_libdir(libdir, group_value):
-    filename = group_value.split('/')[-1]
+def search_filepath_in_libdir(libdir: Optional[str], filepart: str):
+    filename = filepart.split("/")[-1]
 
     for dirpath, dirnames, filenames in os.walk(os.path.expanduser(libdir)):
         for name in filenames:
@@ -102,39 +107,67 @@ def search_filepath_in_libdir(libdir, group_value):
                 return os.path.join(dirpath, name)
 
 
-def findmatch(config, cwd, libdir, strmatch):
-    filepath = None
-    line = column = 1
+def search_filepath(cwd: str, libdir: Optional[str], filepart: str):
+    if os.path.exists(filepart):
+        return (filepart, True)
 
-    from pprint import pprint
-    pprint(["strmatch", strmatch, '\n'])
+    cwd_filepath = os.path.join(cwd, filepart)
+    if os.path.exists(cwd_filepath):
+        return (cwd_filepath, True)
 
-    match = re.match(config['match'], strmatch)
+    if libdir:
+        libdir_filepath = search_filepath_in_libdir(libdir, filepart)
+        if os.path.exists(libdir_filepath):
+            return (libdir_filepath, True)
+
+    return (None, False)
+
+
+def findmatch(config, cwd: str, libdir: Optional[str], io_line: str):
+    regex = config["match"]
+    group_names = config["groups"].split()
+
+    # print("io_line")
+    # print(io_line)
+    # print()
+
+    result = {
+        "match": None,
+        "file": None,
+        "line": 1,
+        "column": 1,
+        "exists": False,
+    }
+
+    match = re.match(regex, io_line)
+
+    # print("match")
+    # print(match)
+    # print()
 
     if match is None:
-        return None, 1, 1
+        return result
 
     groups = [group for group in match.groups() if group is not None]
-    group_names = config['groups'].split()
 
-    pprint(["match", match, '\n'])
-    pprint(["groups", groups, '\n'])
-    pprint(["group_names", group_names, '\n'])
+    # print("groups")
+    # pprint(groups)
+    # print()
 
     for group_value, group_name in zip(groups, group_names):
-        pprint([group_name, group_value, '\n'])
+        if group_name == "file":
+            (filepath, exists) = search_filepath(cwd, libdir, group_value)
+            result["match"] = group_value
+            result["file"] = filepath
+            result["exists"] = exists
 
-        if group_name == 'file':
-            filepath = os.path.join(cwd, group_value)
-            if not os.path.exists(filepath):
-                if libdir:
-                    filepath = search_filepath_in_libdir(libdir, group_value)
-                else:
-                    filepath = None
-        elif group_name == 'line':
-            if group_value:
-                line = group_value
-        elif group_name == 'column':
-            column = group_value
-    return filepath, int(line), int(column)
+        elif group_name == "line":
+            line = group_value.replace(":", "")
+            if line.isdigit():
+                result["line"] = int(line)
 
+        elif group_name == "column":
+            column = group_value.replace(":", "")
+            if column.isdigit():
+                result["column"] = int(column)
+    return result
--- test/test_regex.py
+++ test/test_regex.py
@@ -3,12 +3,12 @@ import os
 from editor_plugin import findmatch
 
 cwd = os.getcwd()
-file = os.path.join(cwd, 'editor_plugin.py')
-libdir = ''
+file = os.path.join(cwd, "editor_plugin.py")
+libdir = ""
 
 
 config = {
-    'command': 'vscodium --goto {filepath}:{line}',
+    "command": "vscodium --goto {filepath}:{line}",
     # 'match': r'([^ \t\n\r\f\v\(:]+?\.([a-zA-Z][\w]{1,20}))([ \n:]|$)([0-9]+)*([ \n:]|$)([0-9]+)*',
     # 'match': r'([^:\(\s]+?\.([a-zA-Z][\w]{1,20}))([ \n:]|$)([0-9]+)*([ \n:]|$)([0-9]+)*',
     # 'match': r'([^:\(\s]+?\.([\w]{1,20}))([\s:]|$)([0-9]+)*([\s:]|$)([0-9]+)*',
@@ -17,44 +17,92 @@ config = {
     # 'match': r'[\(: \n]?([~\/a-zA-Z][^:\(\s]+[\.\w]+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}',
     # 'match': r'([~\/a-zA-Z][^:\(\s]+[\.\w]+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}',
     # 'match': r'([^:\(\s]+[\.\w]+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}',
-    # 'match': r'([^:\(\s]+[\w\-.]+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}',
-    'match': r'([^:\(\s]*[\w\-.]+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}',
-    
-    'groups': 'file _line_separator line _column_separator column',
+    # "match": r"([^:\(\s]+[\w\-.]+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}",
+    # "match": r"([^:\(\s]*[\w\-.]+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}",
+    # "match": r"(([^ !$`&*()+]|(\[ !$`&*()+]))+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}",
+    "match": r"[^a-zA-Z/]*([^\0\t\n\r\f\v\(:'\")]+)(:[0-9]+)?(:[0-9]+)?[^a-zA-Z/]*",
+    "groups": "file line column",
+    # "match": r"(((?=\/)|\.|\.\.)(\/(?=[^/\0])[^/\0]+)*\/?)(:([0-9]+)){0,1}(:([0-9]+)){0,1}",
+    # "match": r"((?:[a-zA-Z]\:){0,1}(?:[\\/][\w.]+){1,})",
+    # "match": r"([^ \t\n\r\f\v:]+?)(:([0-9]+)){0,1}(:([0-9]+)){0,1}",
+    # "match": r"([^ \t\n\r\f\v:]+)(:([0-9]+)){0,1}(:([0-9]+)){0,1}",
 }
 
 
+@pytest.mark.parametrize(
+    "cwd, libdir, strmatch, expected",
+    [
+        (cwd, libdir, file, (file, file, 1, 1)),
+        (cwd, libdir, file + ":", (file, file, 1, 1)),
+        (cwd, libdir, file + ":2", (file, file, 2, 1)),
+        (cwd, libdir, file + ":3:", (file, file, 3, 1)),
+        (cwd, libdir, file + ":4:5", (file, file, 4, 5)),
+        (cwd, libdir, file + ":6:7:", (file, file, 6, 7)),
+        (cwd, libdir, file + ":6:7:", (file, file, 6, 7)),
+    ],
+)
+def test_findmatch_for_existing_file(cwd, libdir, strmatch, expected):
+    assert findmatch(config, cwd, libdir, strmatch) == {
+        "match": expected[0],
+        "file": expected[1],
+        "line": expected[2],
+        "column": expected[3],
+        "exists": True,
+    }
 
 
-@pytest.mark.parametrize('cwd, libdir, strmatch, expected', [
-    (cwd, libdir, file, (file, 1, 1)),
-    (cwd, libdir, file + ':', (file, 1, 1)),
-    (cwd, libdir, file + ':2', (file, 2, 1)),
-    (cwd, libdir, file + ':3:', (file, 3, 1)),
-    (cwd, libdir, file + ':4:5', (file, 4, 5)),
-    (cwd, libdir, file + ':6:7:', (file, 6, 7)),
-    (cwd, libdir, file + ':6:7:', (file, 6, 7)),
-
-
-    # # File doesn't exist
-    (cwd, libdir, file + 'missing', (None, 1, 1)),
-    (cwd, libdir, file + 'missing:0', (None, 0, 1)),
-    (cwd, libdir, file + 'missing:9:8', (None, 9, 8)),
-    (cwd, libdir, file + 'missing:7:6:', (None, 7, 6)),
-    (cwd, libdir, file + 'missing:5:4::3', (None, 5, 4)),
-])
-def test_findmatch(cwd, libdir, strmatch, expected):
-    assert findmatch(config, cwd, libdir, strmatch) == expected
+@pytest.mark.parametrize(
+    "cwd, libdir, strmatch, expected",
+    [
+        (cwd, libdir, file + "missing", (file + "missing", None, 1, 1)),
+        (cwd, libdir, file + "missing:0", (file + "missing", None, 0, 1)),
+        (cwd, libdir, file + "missing:9:8", (file + "missing", None, 9, 8)),
+        (cwd, libdir, file + "missing:7:6:", (file + "missing", None, 7, 6)),
+        (cwd, libdir, file + "missing:5:4::3", (file + "missing", None, 5, 4)),
+    ],
+)
+def test_findmatch_for_non_existing_file(cwd, libdir, strmatch, expected):
+    assert findmatch(config, cwd, libdir, strmatch) == {
+        "match": expected[0],
+        "file": expected[1],
+        "line": expected[2],
+        "column": expected[3],
+        "exists": False,
+    }
 
 
 def test_mix_test():
-    cwd = '/home/sitch/sites/chainapi/chainapi-api'
-    line = '       test/chainapi/domains/apis/schemas/endpoint_parameter_test.exs:32: (test)\n'
-    assert findmatch(config, cwd, libdir, line) == ('test/chainapi/domains/apis/schemas/endpoint_parameter_test.exs', 32, 1)
+    cwd = "/home/sitch/sites/chainapi/chainapi-api"
+    line = "       test/chainapi/domains/apis/schemas/endpoint_parameter_test.exs:32: (test)\n"
+
+    assert findmatch(config, cwd, libdir, line) == {
+        "match": "test/chainapi/domains/apis/schemas/endpoint_parameter_test.exs",
+        "file": "/home/sitch/sites/chainapi/chainapi-api/test/chainapi/domains/apis/schemas/endpoint_parameter_test.exs",
+        "line": 32,
+        "column": 1,
+        "exists": True,
+    }
 
 
-@pytest.mark.skip()
-def test_mix_test():
-    cwd = '/home/sitch/sites/chainapi/chainapi-api'
-    line = 'test/test_regex.py::test_findmatch[/home/sitch/sites/terminator-editor-plugin--/home/sitch/sites/terminator-editor-plugin/editor_plugin.pymissing:0-expected7] ✓73%\n'
-    assert findmatch(config, cwd, libdir, line) == ('/home/sitch/sites/terminator-editor-plugin', 1, 1)
+def test_mix_string_console_output():
+    cwd = "/home/sitch/sites/chainapi/chainapi-api"
+    line = '"/home/sitch/sites/chainapi/chainapi-api/_build/dev/lib/chainapi/.mix"'
+    assert findmatch(config, cwd, libdir, line) == {
+        "match": "/home/sitch/sites/chainapi/chainapi-api/_build/dev/lib/chainapi/.mix",
+        "file": "/home/sitch/sites/chainapi/chainapi-api/_build/dev/lib/chainapi/.mix",
+        "line": 1,
+        "column": 1,
+        "exists": True,
+    }
+
+
+def test_pytest_test():
+    cwd = "/home/sitch/sites/chainapi/chainapi-api"
+    line = "test/test_regex.py::test_findmatch[/home/sitch/sites/terminator-editor-plugin--/home/sitch/sites/terminator-editor-plugin/editor_plugin.pymissing:0-expected7] ✓73%\n"
+    assert findmatch(config, cwd, libdir, line) == {
+        "match": "test/test_regex.py",
+        "file": "test/test_regex.py",
+        "line": 1,
+        "column": 1,
+        "exists": True,
+    }
